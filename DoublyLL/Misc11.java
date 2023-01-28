6b83282a238be8659f58dd0d81e06f569afeff37
Harshita2025 <98738210+acecoder7@users.noreply.github.com>
2023-01-28 17:05:49 +0530

    Eighth commit

--- DoublyLL/Misc11.java
+++ DoublyLL/Misc11.java
@@ -43,6 +43,24 @@ public class Misc11 {
         return head;
     }
 
+    static DNode addLast(DNode head, int ele){
+        if(head==null){
+            DNode nnode = new DNode(ele);
+            head=nnode;
+            return head;
+        }
+        DNode tmp=head;
+        while(tmp.next!=null){
+            tmp=tmp.next;
+        }
+        DNode nnode = new DNode(ele);
+        tmp.next=nnode;
+        nnode.prev=tmp;
+        nnode.next=null;
+
+        return head;
+    }
+
     static DNode removeFirst(DNode head){
         if(head==null || head.next==null){
             head=null;
@@ -71,6 +89,56 @@ public class Misc11 {
         return head;
     }
 
+    static int getFirst(DNode head){
+        if(head==null){
+            return -1;
+        }
+        return head.val;
+    }
+
+    static int getLast(DNode head){
+        if(head==null){
+            return -1;
+        }
+        DNode tmp=head;
+        while(tmp.next!=null){
+            tmp=tmp.next;
+        }
+        return tmp.val;
+    }
+
+    static DNode getAt(DNode head, int idx){
+        if(head==null){
+            return null;
+        }
+        DNode tmp=head;
+        while(idx-->0){
+            tmp=tmp.next;
+        }
+        return tmp;
+    }
+
+    static DNode addAt(DNode head, int idx, int ele){
+        if(head==null || head.next==null || idx==0){
+            addFirst(head, ele);
+        }
+        if(sizeLL(head)==idx){
+            addLast(head, ele);
+        }
+        DNode tmp = getAt(head, idx);
+        DNode pre = tmp.prev;
+        //System.out.println(tmp.val);
+        DNode nnode = new DNode(ele);
+        pre.next=nnode;
+        nnode.prev=pre;
+        nnode.next=tmp;
+        tmp.prev=nnode;
+
+        return head;
+        
+
+    }
+
 
     public static void main(String[] args) {
         DNode start1 = new DNode(8);
@@ -91,7 +159,14 @@ public class Misc11 {
 
         //printLL(addFirst(start1, 91));
         //printLL(removeFirst(start1));
-        printLL(removeLast(start2));
+        //printLL(removeLast(start2));
+        //System.out.println(getFirst(start1));
+        //System.out.println(getLast(start1));
+        //System.out.println(getAt(start1, 2));
+        printLL(addAt(start2, 3, 22));
+
+
+
         
     }
 }
